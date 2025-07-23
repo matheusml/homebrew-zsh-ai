153040e16ddb76b1a767681e3ba665d8fb6478e2
Matheus Lima <matheusml90@gmail.com>
2025-07-23 09:39:39 -0300

    Push v0.5.7

--- Formula/zsh-ai.rb
+++ Formula/zsh-ai.rb
@@ -1,8 +1,8 @@
 class ZshAi < Formula
   desc "Lightweight AI assistant for your terminal"
   homepage "https://github.com/matheusml/zsh-ai"
-  url "https://github.com/matheusml/zsh-ai/archive/refs/tags/v0.5.6.tar.gz"
-  sha256 "5645eab3b0455575f6c9a56b48f2518282656ed1c1826e689026f5d68c68b221"
+  url "https://github.com/matheusml/zsh-ai/archive/refs/tags/v0.5.7.tar.gz"
+  sha256 "3f4e657845a2ffcb5956755f427eed1e7ef0b247dc12e2ff4686e7ff38f5f798"
   license "MIT"
 
   def install
